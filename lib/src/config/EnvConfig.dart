55ba77717bf3b2359bf4b7fcbf8a460b7b068b06
Khaya Verna <khayav@touchsides.com>
2021-11-06 11:44:33 +0200

    Fix api now address

--- lib/src/config/EnvConfig.dart
+++ lib/src/config/EnvConfig.dart
@@ -12,6 +12,9 @@ class EnvConfig {
   String mapBoxKey;
   int connectTimeOut;
   int receiveTimeout;
+  String wavePubKey;
+  String waveEncryptKey;
+  String waveSecretKey;
 
   EnvConfig(
       {this.appKey,
@@ -24,31 +27,25 @@ class EnvConfig {
       this.tweetApiKey,
       this.tweetApiKeySecret,
       this.tweetBearerToken,
-      this.mapBoxKey});
+      this.mapBoxKey,
+      this.wavePubKey,
+      this.waveEncryptKey,
+      this.waveSecretKey});
 
   factory EnvConfig.fromJson(Map<String, dynamic> json) => EnvConfig(
-      appKey: json['appKey'] as String,
-      appName: json['appName'] as String,
-      appVersion: json['appVersion'] as String,
-      baseUrl: json['baseUrl'] as String,
-      connectTimeOut: json['connectTimeOut'] as int,
-      receiveTimeout: json['receiveTimeout'] as int,
-      databaseName: json['databaseName'],
-      tweetApiKey: json['tweetKey'],
-      tweetApiKeySecret: json['tweetKeySecret'],
-      tweetBearerToken: json['tweetBearerToken'],
-      mapBoxKey: json['mapBoxKey']);
-
-  Map<String, dynamic> toJson() {
-    return {
-      "appKey": this.appKey,
-      "appName": this.appName,
-      "appVersion": this.appVersion,
-      "databaseName": this.databaseName,
-      "baseUrl": this.baseUrl,
-      "connectTimeOut": this.connectTimeOut,
-      "receiveTimeout": this.receiveTimeout,
-      "mapBoxKey": this.mapBoxKey,
-    };
-  }
+        appKey: json['appKey'] as String,
+        appName: json['appName'] as String,
+        appVersion: json['appVersion'] as String,
+        baseUrl: json['baseUrl'] as String,
+        connectTimeOut: json['connectTimeOut'] as int,
+        receiveTimeout: json['receiveTimeout'] as int,
+        databaseName: json['databaseName'],
+        tweetApiKey: json['tweetKey'],
+        tweetApiKeySecret: json['tweetKeySecret'],
+        tweetBearerToken: json['tweetBearerToken'],
+        mapBoxKey: json['mapBoxKey'],
+        wavePubKey: json['wavePubKey'],
+        waveEncryptKey: json['waveEncryptKey'],
+        waveSecretKey: json['waveSecretKey'],
+      );
 }
