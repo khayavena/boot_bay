2d0ffad1d02fe3cc5b12f291370e252b52aae752
Khaya Verna <khayav@touchsides.com>
2022-04-05 12:37:15 +0200

    Fix everything

--- lib/src/pages/checkout/drop_in/yoco_web_drop_in_page.dart
+++ lib/src/pages/checkout/drop_in/yoco_web_drop_in_page.dart
@@ -2,12 +2,13 @@ import 'dart:convert';
 
 import 'package:bootbay/src/enum/loading_enum.dart';
 import 'package:bootbay/src/pages/checkout/viewmodel/yoco_view_model.dart';
+import 'package:bootbay/src/wigets/shared/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:provider/provider.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-import '../../../helpers/ResColor.dart';
+import '../../../helpers/ResFont.dart';
 import '../../../wigets/shared/loading/color_loader_4.dart';
 
 class YocoWebDropInPage extends StatefulWidget {
@@ -30,16 +31,14 @@ class YocoWebDropInPage extends StatefulWidget {
 class _YocoWebDropInPageState extends State<YocoWebDropInPage> {
   static const DropInChannel = "DropInChannel";
   static const DropInStarted = "started";
-  YocoViewModel yocoViewModel;
 
   @override
   void initState() {
     SchedulerBinding.instance.addPostFrameCallback((_) {
-      yocoViewModel = Provider.of<YocoViewModel>(
+      Provider.of<YocoViewModel>(
         context,
         listen: false,
-      );
-      yocoViewModel.buildUrl(widget.finalAmount, widget.currency);
+      ).buildUrl(widget.finalAmount, widget.currency);
     });
     super.initState();
   }
@@ -47,60 +46,79 @@ class _YocoWebDropInPageState extends State<YocoWebDropInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: primaryWhite,
-        bottomOpacity: 0.0,
-        elevation: 0.0,
-        automaticallyImplyLeading: true,
-        title: Text(
-          "Authorise Credit Card",
-          style: TextStyle(color: secondaryBlueColor),
-        ),
-      ),
+      appBar: CustomAppBar.build("Authorise Credit Card", context),
       body: loadDropIn(),
     );
   }
 
   Widget loadDropIn() {
     return Consumer<YocoViewModel>(
-        builder: (BuildContext context, vm, Widget child) {
-      switch (vm.loader) {
+        builder: (BuildContext context, yocoViewModel, Widget child) {
+      switch (yocoViewModel.loader) {
         case Loader.error:
           break;
       }
       return Stack(
         children: [
           Container(
-            child: vm.finalUrl == null
+            child: yocoViewModel.finalUrl == null
                 ? WidgetLoader()
-                : WebView(
-                    initialUrl: vm.finalUrl,
-                    javascriptMode: JavascriptMode.unrestricted,
-                    onPageFinished: (string) async {},
-                    onWebViewCreated: (controller) {},
-                    javascriptChannels: Set.from([
-                      JavascriptChannel(
-                          name: DropInChannel,
-                          onMessageReceived: (JavascriptMessage result) async {
-                            switch (result.message) {
-                              case DropInStarted:
-                                vm.setState(Loader.busy);
-                                break;
-                              default:
-                                var json = jsonDecode(result.message);
-                                String error = json['error'] ?? null;
-                                if (error == null) {
-                                  vm.fromJson(json);
-                                  Navigator.pop(context);
-                                } else {
-                                  vm.errorMessage = error;
-                                }
-                            }
-                          }),
-                    ]),
+                : Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Center(
+                            child: Text(
+                          widget.currency +
+                              "-" +
+                              widget.finalAmount.toStringAsFixed(2).toString(),
+                          style: TextStyle(
+                            color: Color(0xff2783a9),
+                            fontSize: 25,
+                            fontWeight: mediumFont,
+                            fontStyle: FontStyle.normal,
+                            letterSpacing: -0.6400000000000001,
+                          ),
+                        )),
+                        SizedBox(height: 16),
+                        Expanded(
+                          child: WebView(
+                            initialUrl: yocoViewModel.finalUrl,
+                            javascriptMode: JavascriptMode.unrestricted,
+                            onPageFinished: (string) async {},
+                            onWebViewCreated: (controller) {},
+                            javascriptChannels: Set.from([
+                              JavascriptChannel(
+                                  name: DropInChannel,
+                                  onMessageReceived:
+                                      (JavascriptMessage result) async {
+                                    switch (result.message) {
+                                      case DropInStarted:
+                                        yocoViewModel.setState(Loader.busy);
+                                        break;
+                                      default:
+                                        var json = jsonDecode(result.message);
+                                        String error = json['error'] ?? null;
+                                        if (error == null) {
+                                          yocoViewModel.fromJson(json);
+                                          Navigator.pop(context);
+                                        } else {
+                                          yocoViewModel.errorMessage = error;
+                                          yocoViewModel.setState(Loader.error);
+                                        }
+                                    }
+                                  }),
+                            ]),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
           ),
-          vm.loader == Loader.busy ? WidgetLoader() : SizedBox(height: 0)
+          yocoViewModel.loader == Loader.busy
+              ? WidgetLoader()
+              : SizedBox(height: 0)
         ],
       );
     });
